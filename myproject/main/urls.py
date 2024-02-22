6546c17e43521e36d7b2dfd876d9fdcb700ac4e6
Alyona Dombrina <al.dombrina@gmail.com>
2024-02-22 08:58:10 +0300

    commit end

--- myproject/main/urls.py
+++ myproject/main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about-us', views.about)
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('create', views.create, name='create')
 ]
